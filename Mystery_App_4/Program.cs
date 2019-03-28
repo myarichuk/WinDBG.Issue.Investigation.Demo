881f6f6de4df10a7b46a82d97b3876a9d1676387
Michael Yarichuk <michael.yarichuk@gmail.com>
2019-03-29 00:12:59 +0300

    better more informative WinDBG command tutorials, add AccessViolationException tutorial

---
+++ Mystery_App_4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Mystery_App_4
+{
+    class Program
+    {
+        private static void TryToAccessInvalidMemory()
+        {
+            var ptr = Marshal.AllocHGlobal(1024);
+            Marshal.WriteByte(ptr, 65536,123); //try to access unallocated memory            
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Process ID: {Process.GetCurrentProcess().Id}");
+            Console.Write("Press any key to start the process. Attach WinDBG before continuing..");
+            Console.ReadKey();
+
+            TryToAccessInvalidMemory();
+
+            Console.WriteLine("This should not be seen on screen, because the process will crash with AccessViolationException...");
+        }
+    }
+}
