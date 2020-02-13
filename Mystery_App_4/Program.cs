d6b3d40a1f423911623a592a6394d0fbb83dd550
Michael Yarichuk <michael.yarichuk@gmail.com>
2020-02-13 14:51:27 +0200

    improvements to the way the exception is thrown

--- Mystery_App_4/Program.cs
+++ Mystery_App_4/Program.cs
@@ -6,10 +6,17 @@ namespace Mystery_App_4
 {
     class Program
     {
-        private static void TryToAccessInvalidMemory()
+        private static void ThrowCorruptedStateException()
         {
-            var ptr = Marshal.AllocHGlobal(1024);
-            Marshal.WriteByte(ptr, 65536,123); //try to access unallocated memory            
+            try
+            {
+                var ptr = new IntPtr(123);
+                Marshal.StructureToPtr(123, ptr, true);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
         }
 
         static void Main(string[] args)
@@ -18,7 +25,7 @@ namespace Mystery_App_4
             Console.Write("Press any key to start the process. Attach WinDBG before continuing..");
             Console.ReadKey();
 
-            TryToAccessInvalidMemory();
+            ThrowCorruptedStateException();
 
             Console.WriteLine("This should not be seen on screen, because the process will crash with AccessViolationException...");
         }
